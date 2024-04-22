de4b55784a00ca442c44dad2242d8eb773e80073
Rudy Gabuco Jr <140367514+rgabuco@users.noreply.github.com>
2024-04-22 09:21:10 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,27 +24,27 @@ document.addEventListener("DOMContentLoaded", function() {
             }, 200);
         });
     });
+});
 
-  const dropbtn = document.querySelector('.dropbtn');
-  const dropdownContent = document.querySelector('.dropdown-content');
+const dropbtn = document.querySelector('.dropbtn');
+const dropdownContent = document.querySelector('.dropdown-content');
 
-  /* Event listener for clicking the Cheat Sheet dropdown button */
-  dropbtn.addEventListener('click', function() {
-      if (dropdownContent.style.display === 'block') {
-          dropdownContent.style.display = 'none';
-      } else {
-          dropdownContent.style.display = 'block';
-      }
-  });
+/* Event listener for clicking the Cheat Sheet dropdown button */
+dropbtn.addEventListener('click', function() {
+  if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+  } else {
+      dropdownContent.style.display = 'block';
+  }
+});
 
-  /* Close the dropdown when clicking outside of it */
-  window.addEventListener('click', function(event) {
-      if (!event.target.matches('.dropbtn')) {
-          dropdownContent.style.display = 'none';
-      }
-  });
-  /* Event listener for "Download CV" button */
-  document.getElementById("downloadBtn").addEventListener("click", function() {
-    window.open('../assets/rudy-cv.pdf', "_blank");
-  });
+/* Close the dropdown when clicking outside of it */
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      dropdownContent.style.display = 'none';
+  }
+});
+/* Event listener for "Download CV" button */
+document.getElementById("downloadBtn").addEventListener("click", function() {
+window.open('../assets/rudy-cv.pdf', "_blank");
 });
